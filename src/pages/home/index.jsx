7a442c0b71dbf3a4826fb4f029d3d5078e3e44a0
Munnawar <munawwarh48@gmail.com>
2025-08-21 02:29:36 +0530

    Added Loader again

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -13,10 +13,10 @@ import Navbar from "@/components/layout/Navbar";
 import ChizelEcosystemSection from "./sections/ChizelEcosystemSection";
 
 const HomePage = () => {
-  // const [isPageLoading, setIsPageLoading] = useState(true);
-  // if (isPageLoading) {
-  //   return <Loader setIsLoading={setIsPageLoading} />;
-  // }
+   const [isPageLoading, setIsPageLoading] = useState(true);
+   if (isPageLoading) {
+     return <Loader setIsLoading={setIsPageLoading} />;
+   }
   return (
     <>
       <Navbar />
